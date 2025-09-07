7f0cf8c58cf8405c589d393948bd793b37d7a5f0
YaninaA-V <JaninaVit92@yandex.ru>
2025-09-07 22:50:08 +0300

    Первый коммит_who_checkFilesize

--- backend/src/middlewares/check-filesize.ts
+++ backend/src/middlewares/check-filesize.ts
@@ -2,18 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import BadRequestError from "../errors/bad-request-error"; 
 
 export const checkFilesize = (req: Request, res: Response, next: NextFunction) => {
-    console.log('checkFilesize middleware called'); 
     if (!req.file) {
-        console.log('No file found'); 
         return next(new BadRequestError('File not uploaded'));
     }
 
     const minSizeBytes = 2 * 1024;
     if (req.file.size < minSizeBytes) {
-        console.log('File too small:', req.file.size); 
         return next(new BadRequestError(`File is too small. Minimum size is ${minSizeBytes} bytes.`));
     }
-
-    console.log('File passed validation'); 
     next();
 };
--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
-import { checkFilesize } from '../middlewares/check-filesize'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'),checkFilesize, uploadFile)
+uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
 
 export default uploadRouter
