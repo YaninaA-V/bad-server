37db139ea49b990075b77bb4158226d383e7eddc
YaninaA-V <JaninaVit92@yandex.ru>
2025-09-07 22:31:12 +0300

    Первый коммит_logs

--- backend/src/middlewares/check-filesize.ts
+++ backend/src/middlewares/check-filesize.ts
@@ -2,15 +2,18 @@ import { NextFunction, Request, Response } from "express";
 import BadRequestError from "../errors/bad-request-error"; 
 
 export const checkFilesize = (req: Request, res: Response, next: NextFunction) => {
+    console.log('checkFilesize middleware called'); 
     if (!req.file) {
-        return next(new BadRequestError('File not uploaded')); 
+        console.log('No file found'); 
+        return next(new BadRequestError('File not uploaded'));
     }
 
-    const minSizeBytes = 2 * 1024; // 2KB
-
+    const minSizeBytes = 2 * 1024;
     if (req.file.size < minSizeBytes) {
-        return next(new BadRequestError(`File is too small. Minimum size is ${minSizeBytes} bytes.`)); 
+        console.log('File too small:', req.file.size); 
+        return next(new BadRequestError(`File is too small. Minimum size is ${minSizeBytes} bytes.`));
     }
 
-    next(); 
+    console.log('File passed validation'); 
+    next();
 };
