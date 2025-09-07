ab68055bfadf8d532fca5c9d0bdb7108009d5b02
YaninaA-V <JaninaVit92@yandex.ru>
2025-09-07 21:30:59 +0300

    Первый коммит_limits

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -58,7 +58,7 @@ export default multer({
     storage, 
     fileFilter,
     limits: {
-        fileSize: 10 * 1024 * 1024,
+        fileSize: 2 * 1024,
         files: 1, 
         fieldSize: 10 * 1024 * 1024 
     }
